46372d9f6be02e817a8a0afdfb108e99f82cb3fd
Alla Chybisova <allachybisova@gmail.com>
2021-06-01 21:05:11 +0300

    hellow 2

--- src/com/telran/Application.java
+++ src/com/telran/Application.java
@@ -3,5 +3,6 @@ package com.telran;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
+        System.out.println("Hello Alla!!!");
     }
 }
