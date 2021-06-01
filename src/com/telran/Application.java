3a9bb5cf46eebd235d002bc99effc2b9608a8223
Alla Chybisova <allachybisova@gmail.com>
2021-06-01 21:26:01 +0300

    konflict

--- src/com/telran/Application.java
+++ src/com/telran/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
         System.out.println("Hello Alla!!!");
-         System.out.println("David");
+        System.out.println("Alla");
     }
 }
